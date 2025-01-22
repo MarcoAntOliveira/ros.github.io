2f63f7b210c7ca73cc8f195a151ab8f3e9e46a7b
MarcoAntOliveira <marcoolivera096@gmail.com>
2025-01-22 12:52:11 -0300

    atualizando conteudo

--- doc/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from doc import views
-
-urlpatterns = [
-   path('https://marcoantoliveira.github.io/ros.github.io/', views.index, name = "index"),
-]
